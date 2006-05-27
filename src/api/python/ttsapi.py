d96a07d846dcaa47a96529a8a50ef6fcafa1332a
hanke <hanke>
2006-05-27 21:14:48 +0000

    Initial import

--- src/api/python/ttsapi.py
+++ src/api/python/ttsapi.py
@@ -18,7 +18,7 @@
 # the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 # 
-# $Id: ttsapi.py,v 1.1 2006-05-27 21:14:51 hanke Exp $
+# $Id: ttsapi.py,v 1.1.1.1 2006-05-27 21:14:51 hanke Exp $
  
 """Python implementation of TTS API"""
 
