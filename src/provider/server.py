eb62b55da8509700d4495e1a6765615e5882975b
hanke <hanke>
2006-08-09 12:06:04 +0000

    Use the configuration class. (serve_client): Implementation over sockets. (main): Initialize logging, bind socket, wait for connections and start new threads.

--- src/provider/server.py
+++ src/provider/server.py
@@ -16,58 +16,73 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this package; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 # 
-# $Id: server.py,v 1.1 2006-07-25 12:56:17 hanke Exp $
+# $Id: server.py,v 1.2 2006-08-09 12:06:04 hanke Exp $
 
 import sys
 import socket
 import thread
 
-import provider
+# TTS API module
 import ttsapi
 
-class configuration(object):
-    port = 6562
-    max_simultaneous_connections = 5
+# Provider (core of implementation)
+import provider
 
-def serve_client(socket):
+# Logging object
+from logs import log
+
+# Configuration object
+# Automatically fills conf with default values and parses command line options
+from configuration import conf
+
+def serve_client(method, socket=None):
     """Runs one connection to TTS API Provider
     
     Arguments:
-    socket -- the client socket of the connection"""
+    method -- currently only 'socket'
+    socket -- if method = 'socket', the client socket of the connection"""
 
-    p = provider.Provider()    
-    connection = ttsapi.server.TCPConnection(provider=p, client_socket=socket)
-
+    if method == 'socket':
+        print socket
+        assert socket != None
+        p = provider.Provider()    
+        connection = ttsapi.server.TCPConnection(provider=p,
+                                                 client_socket=socket,
+                                                 logger=log)
     while True:
-        connection.process_input()
-    
-    # When the end of the world comes...
-    socket.close()
-    
+        try:
+            connection.process_input()
+        except ttsapi.server.ClientGone:
+            break
+    return
 
 def main():
-    """Initialization, configuration reading, start of server"""
-    # TODO: Parse command line options
+    """Initialization, configuration reading, start of server
+    over sockets on given port"""
+
     # TODO: Check/Create pid file
     # TODO: Register signals
     #   SIGINT, SIGPIPE?, others?
-
-    print "Server starting"
+    
+    log.info("Starting server")
     
     # Create server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("", configuration.port))
-    server_socket.listen(configuration.max_simultaneous_connections)
-    print "Waiting for connections"
+    server_socket.bind(("", conf.port))
+    server_socket.listen(conf.max_simultaneous_connections)
+    
+    log.init_stage2(conf)
+    log.info("Configuration loaded")
+    
+    log.info("Waiting for connections")
     
     while True:
         (client_socket, address) = server_socket.accept()        
-        thread.start_new_thread(serve_client, (client_socket,))
-      
+        thread.start_new_thread(serve_client, ('socket',client_socket))
 
 print __name__
 
