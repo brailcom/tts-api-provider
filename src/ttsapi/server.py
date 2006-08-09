00955bab1066e13201af6809f0b94661639bb3e6
hanke <hanke>
2006-08-09 12:05:33 +0000

    (ClientGone): New exception. (TCPConnection._quit): New method. (TCPConnection.__init__): New parameter 'logger'. Log communication. (TCPConnection.__init__): Typos in self.commands_map. (TCPConnection.__init__): self.errors_map : new variable. (TCPConnection._cmd_matches): Docstring. (TCPConnection._report_error): New method. (TCPConnection.process_input): Exception handling through self.errors_map.

--- src/ttsapi/server.py
+++ src/ttsapi/server.py
@@ -15,18 +15,27 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this package; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 # 
 # $Id: ttsapi.py,v 1.3 2006/07/03 20:56:57 hanke Exp
 
 import connection
 from structures import *
+from errors import *
+
+class ClientGone(Exception):
+    """Raised when connection with client is terminated"""
 
 class TCPConnection(object):
     """TTS API on server side"""
 
-    def __init__(self, provider, client_socket):
+    def _quit(self):
+            #TODO: self.provider.quit()
+            self.conn.close()
+            raise ClientGone()
+
+    def __init__(self, provider, client_socket, logger):
         """Init the server side object for a new connection
         
         Arguments:
@@ -34,12 +43,15 @@ class TCPConnection(object):
         functions defined bellow in commands_map
         client_socket -- socket for communication with client"""
 
-        self.conn = connection.SocketConnection(socket=client_socket)
-        
+        self.provider = provider
+        self.conn = connection.SocketConnection(socket=client_socket, logger=logger)
+        self.logger = logger
+    
         self.commands_map = [
             (('LIST', 'DRIVERS'),
              {
-            'function' : provider.drivers
+            'function'  :  provider.drivers,
+            'reply' :  (201, 'OK LIST OF DRIVERS SENT')
             }),
 
             (('DRIVER', 'CAPABILITIES', ('driver_id', str)),
@@ -51,20 +63,20 @@ class TCPConnection(object):
 
             (('LIST', 'VOICES', ('driver_id', str)),
              {
-            'function_id': provider.voices,
+            'function': provider.voices,
             'reply': (203,'OK LIST OF VOICES SENT')
             }),
             
             (('SAY', 'DEFERRED', ('message_id', int)),
              {
-            'function_id': provider.say_deferred,
+            'function': provider.say_deferred,
             'reply': (205, 'OK MESSAGE RECEIVED')
             }),
             
             (('SAY', 'DEFERRED', ('message_id', int), 'FROM', 'POSITION',
               ('position', int), ('position_type', str)),
              {
-            'function_id': provider.say_deferred,
+            'function': provider.say_deferred,
             'reply': (205, 'OK MESSAGE RECEIVED')
             }),
             
@@ -196,19 +208,32 @@ class TCPConnection(object):
             'function': provider.set_audio_retrieval_destination
             }),
             
-            (('HELP'),
+            (('HELP',),
              {
             'function': None
             }),
-            
-            (('QUIT'),
+            (('QUIT',),
              {
-            'function': None
-            })         
+            'function':  self._quit,
+            'reply': None
+            }) 
             ]
-        self.provider = provider
-
+        
+        self.errors_map = (
+            (UnknownError, (300, 'UNKNOWN ERROR')),
+            (ErrorNotSupportedByDriver, (301, 'NOT SUPPORTED BY DRIVER')),
+            (ErrorNotSupportedByServer, (302, 'NOT SUPPORTED BY SERVER')),
+            (ErrorAccessToDriverDenied, (303, 'DRIVER ACCESS DENIED')),
+            (ErrorInternal, (304, 'INTERNAL ERROR')),
+            (ErrorInvalidCommand, (400, 'INVALID COMMAND')),
+            (ErrorInvalidArgument, (401, 'INVALID ARGUMENT')),
+            (ErrorMissingArgument, (402, 'MISSING ARGUMENT')),
+            (ErrorInvalidParameter, (403, 'INVALID PARAMETER')),
+            (ErrorWrongEncoding, (404, 'ENCODING ERROR'))
+            )
+        
     def _driver_capabilities_reply(self, result):
+        """Driver capabilities reply hook"""
 
         capabilities = result.attributes_dict()
 
@@ -219,11 +244,13 @@ class TCPConnection(object):
             elif isinstance(value, bool):
                 reply += [key, value.lower()]
             else:
-                reply += [key, str(value)]                                                
+                reply += [key, str(value)]
         return reply
-    
-
+        
     def _cmd_matches(self, command, template):
+        """Compare command and template, return
+        True if they match, otherwise False""" 
+        
         if len(command) != len(template):
             return False
         
@@ -238,8 +265,25 @@ class TCPConnection(object):
                        
         return True            
 
-     
+    def _report_error(self, error):
+        """Report error on the connection according to
+        self.errors_map"""
+        for entry in self.errors_map:
+            if isinstance(error, entry[0]):
+                err_code = entry[1][0]
+                err_reply = entry[1][1]
+                err_detail = error.detail()
+                break
+        else:
+            err_code = 300
+            err_reply = "UNKNOWN ERROR"
+            err_detail = None
+
+        self.conn.send_reply(err_code, err_reply, err_detail)
+            
     def process_input(self):
+        """Read one line of input and process it, calling the
+        appropriate functions as defined in self."""
         cmd = self.conn.receive_line()
         # Check if we didn't get only data
         if cmd == None:
@@ -251,7 +295,7 @@ class TCPConnection(object):
             if cmdl[1] == 'text':
                 self.last_cmd = cmd
                 self.conn.data_transfer_on()
-                self.conn.send_reply('204 OK RECEIVING DATA')
+                self.conn.send_reply(204, 'OK RECEIVING DATA')
                 return
         elif cmdl[0] == '.':
             self.conn.data_transfer_off()
@@ -260,12 +304,13 @@ class TCPConnection(object):
             self.conn.send_reply('204 OK MESSAGE RECEIVED')
 
         for pos in range(0, len(self.commands_map)):
+            # print self.commands_map[pos]
             template, action = self.commands_map[pos]
             if self._cmd_matches(cmd, template):
-                break;
+                break
         else:
-            # Raise exception
-            print "ERROR, Unknown command"
+            self._report_error(ErrorInvalidCommand())
+            return
 
         arg_dict = {}
         for i in range(0, len(template)):
@@ -275,28 +320,27 @@ class TCPConnection(object):
                 arg_dict[atom[0]] = arg_type(cmd[i])
 
         if not action.has_key('function'):
-            raise NotImplementedError
+            self._report_error(ErrorInternal());
         else:
             function = action['function']
 
         if function == None:
-            self.conn.send_reply(301, "ERR UNKNOWN COMMAND")
+            pass
         else:
-            # TODO: try (exception handling)
-            print arg_dict
-            result = action['function'](self.provider, **arg_dict)
-            # catch and print error reply on socket
-
+            try:
+                result = function(**arg_dict)
+            except Error, err:
+                self._report_error(err);
+                return
+    
+        if action.has_key('function_post_hook'):
+            action['function_post_hook']()  
+            
         if action.has_key('reply_hook'):
             result = action['reply_hook'](result)
         else:
             reply = action['reply']
-            if (not isinstance(result, list)) and (not result == None):
-                result = [result]
-        self.conn.send_reply(reply[0], reply[1], result)
-        
-
-
-
-         
-    
+            if reply != None:
+                if (not isinstance(result, list)) and (not result == None):
+                    result = [result]
+                self.conn.send_reply(reply[0], reply[1], result)
